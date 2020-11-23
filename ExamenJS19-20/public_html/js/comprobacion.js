80e06f6794e55819a7912852d5805f3cf282c92a
vengadoana <geralejo95@gmail.com>
2020-11-23 17:55:46 +0100

    progreso jquery

--- ExamenJS19-20/public_html/js/comprobacion.js
+++ ExamenJS19-20/public_html/js/comprobacion.js
@@ -1,44 +1,104 @@
 
-$(function(){
-    $('#prueba').click(function() {
-        alert("Hello");
+//$(function(){
+//    $('#prueba').click(function() {
+//        alert("Hello");
+//    });
+//});
+//
+//window.onload = function() {
+//    if (window.jQuery) {  
+//        // jQuery is loaded  
+//        alert("Yeah!");
+//    } else {
+//        // jQuery is not loaded
+//        alert("Doesn't Work");
+//    }
+//}
+
+$(document).ready(function () {
+
+    var inputsFocus = $("input");
+
+    inputsFocus.focusin(enfocado(inputsFocus));
+    inputsFocus.focusout(comprobarVacio(inputsFocus));
+
+    var elemInte = document.getElementById('numIntegrantes');
+
+    $(elemInte).bind("focusout", function () {
+        //alert("hola");
+        var numInte = parseInt($(elemInte).val());
+        var padreDiv = document.getElementById('componentes');
+        var modalidad = $('input[name=modalidad]:checked', '#registroForm').val();
+
+        //vacia componentes
+        $(padreDiv).empty();
+
+        if (modalidad === "cuarteto" && numInte > 5) {
+            console.log("cuartetoMal");
+            marcaError(elemInte, "Numero de Integrantes no valido");
+
+
+        } else if ((modalidad === "chirigota" || modalidad === "comparsa") && numInte > 13) {
+            console.log("chirigotamal");
+            marcaError(elemInte, "Numero de Integrantes no valido");
+
+        } else if (modalidad === "coro" && numInte > 30) {
+            console.log("coro mal");
+            marcaError(elemInte, "Numero de Integrantes no valido");
+
+        } else {
+
+            for (var i = 0; i < numInte; i++) {
+                console.log("todobien");
+                var newInput = document.createElement("input");
+                $(newInput).attr("type", "text");
+                $(newInput).attr("class", "componente");
+                var newLabel = document.createElement("label");
+                $(newLabel).text("Componente" + (i + 1));
+                $(padreDiv).append(newInput);
+                $(newInput).before(newLabel);
+            }
+
+        }
     });
+
 });
 
-function marcaError(elemento, mensaje){
-    
+function marcaError(elemento, mensaje) {
+
     var elemType = $(elemento).is("span");
+    //cosole.log("marcaError");
     var clase = $(elemento).attr("class");
-    
-    if(!elemType && clase !== "error"){
+    alert($(elemento).name);
+    if (!elemType && clase !== "error") {
         $(elemento).css({'border': '2px, solid, red'});
         var newSpan = document.createElement("span");
         $(newSpan).addClass("error");
         $(newSpan).text(mensaje);
         $(elemento).append(newSpan);
     }
-    
+
 }
 
-function enfocado(elemento){
-    
+function enfocado(elemento) {
+
     $(elemento).css({'border': 'solid, 2px, green'});
-    
+
     var spanHijo = $(elemento).next();
-    
-    if($(spanHijo).is("span")){
+
+    if ($(spanHijo).is("span")) {
         $(spanHijo).remove().fadeOut("slow");
     }
-    
+
 }
 
-function comprobarVacio(elemento){
-    
-    if($(elemento).text() === ""){
+function comprobarVacio(elemento) {
+
+    if ($(elemento).text() === "") {
         marcaError(elemento, "error");
-    }else{
-        $(elemento).css('border','none');
+    } else {
+        $(elemento).css('border', 'none');
     }
-    
+
 }
 
